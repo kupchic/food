b24d93538d494b6489d315febcc8b3467a93def9
Kupchenya Aleksandr <kup4enya1999@gmail.com>
2020-10-14 12:50:59 +0300

    fix problems

--- scripts/script.js
+++ scripts/script.js
@@ -5,27 +5,40 @@ menuButton.addEventListener('click', () => {
   document.getElementsByTagName('html')[0].classList.toggle('overfl');
   if (menuButton.className.includes('menu-button__active')) {
     document.getElementById('mobile-menu').style.display = 'block';
-
+    window.addEventListener('click', () => {
+      let target = event.target;
+      if (target.id === 'mobile-menu') {
+        document.getElementById('mobile-menu').style.display = 'none';
+        menuButton.classList.remove('menu-button__active');
+      }
+    });
   } else document.getElementById('mobile-menu').style.display = 'none';
 });
-window.addEventListener('click', () => {
-  let target = event.target;
-  if (target.id === 'mobile-menu') {
-    document.getElementById('mobile-menu').style.display = 'none';
-    menuButton.classList.remove('menu-button__active');
-  }
+
+
+
+$('.review-slider').slick({
+  slidesToShow: 1,
+  prevArrow: '.prev-btn',
+  nextArrow: '.next-btn',
 });
 
 
-if (document.body.clientWidth >770) {
-  $('.slider-wrp_diner').slick({
-    // slidesToShow: 4,
-    prevArrow: '.slider-one-prev',
-    nextArrow: '.slider-one-next'
-  });
-  $('.slider-wrp_menu').slick({
-    // slidesToShow: 4,
-    prevArrow: '.slider-two-prev',
-    nextArrow: '.slider-two-next'
-  });
-}
+$('.slider-wrp_diner').slick({
+  slidesToShow: 4,
+  prevArrow: '.slider-one-prev',
+  nextArrow: '.slider-one-next',
+  responsive: [{
+    breakpoint: 1170,
+    settings: "unslick"
+  }]
+});
+$('.slider-wrp_menu').slick({
+  slidesToShow: 4,
+  prevArrow: '.slider-two-prev',
+  nextArrow: '.slider-two-next',
+  responsive: [{
+    breakpoint: 1170,
+    settings: "unslick"
+  }]
+});
